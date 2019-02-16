219bf6b6154ba8c9a929311f88ce92052ccf1355
Pratyush Singh <singh.pratyush96@gmail.com>
2019-02-16 17:22:01 +0530

    Add inputs

--- src/class-components/index.js
+++ src/class-components/index.js
@@ -1,11 +1,38 @@
 import React from 'react';
 
 export default class ClassComponent extends React.Component {
-    render() {
-        return (
-            <div>
-                Hi, I am the class component.
-            </div>
-        );
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      inputValue: '',
     }
+
+    this.handleValueChange = this.handleValueChange.bind(this);
+  }
+
+  handleValueChange(event) {
+    this.setState({
+      inputValue: event.target.value,
+    });
+  }
+
+  render() {
+    return (
+      <div>
+        <div>
+          Hi, I am the class component.
+        </div>
+
+        <div>
+          <input
+            type="number"
+            value={this.state.inputValue}
+            onChange={this.handleValueChange}
+          />
+          <button>Update!</button>
+        </div>
+      </div>
+    );
+  }
 }
--- src/functional-components/index.js
+++ src/functional-components/index.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const FunctionalComponent = () => {
+  const [inputValue, setInputValue] = useState('');
   return (
     <div>
-      Hi, I am the functinal component.
+      <div>
+        Hi, I am the functional component.
+      </div>
+
+      <div>
+        <input
+          type="number"
+          value={inputValue}
+          onChange={e => setInputValue(e.target.value)}
+        />
+        <button>Update!</button>
+      </div>
     </div>
   );
 };
