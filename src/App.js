ba38ed2df7662bfc7887fbf907858d2b855c311d
Pratyush Singh <singh.pratyush96@gmail.com>
2019-02-16 19:18:55 +0530

    Show side by side

--- src/App.js
+++ src/App.js
@@ -1,42 +1,24 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import ClassComponent from './class-components';
 import FunctionalComponent from './functional-components';
 
-const componentTypes = {
-  class: 'class',
-  functional: 'functional',
+const borderStyle = {
+  borderRight: '1px solid rgba(0, 0, 0, 0.12)',
+  height: '100vh',
 };
 
-const App = () => {
-  const [activeComponent, setActiveComponent] = useState(componentTypes.class);
-
-  return (
-    <div className="uk-container uk-margin-top">
-      <div className="uk-flex">
-        <button
-          onClick={() => setActiveComponent(componentTypes.class)}
-          className={`uk-button uk-button-${(activeComponent === componentTypes.class) ? 'primary' : 'default'} uk-margin-small-right`}
-        >
-          Class Component
-        </button>
-        <button
-          onClick={() => setActiveComponent(componentTypes.functional)}
-          className={`uk-button uk-button-${(activeComponent === componentTypes.functional) ? 'primary' : 'default'}`}
-        >
-          Functional Component
-        </button>
+const App = () => (
+  <div className="uk-container">
+    <div className="uk-grid">
+      <div className="uk-width-1-2" style={borderStyle}>
+        <ClassComponent />
       </div>
-
-      <div className="uk-margin-top">
-        {
-          activeComponent === componentTypes.class
-            ? <ClassComponent />
-            : <FunctionalComponent />
-        }
+      <div className="uk-width-1-2">
+        <FunctionalComponent />
       </div>
     </div>
-  )
-};
+  </div>
+);
 
 export default App;
